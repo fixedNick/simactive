6cb61fda2b4291c7f592c0189debd1e7306293d8
fixedNick <kkos44911@gmail.com>
2024-04-03 21:03:15 +0300

    fixed service struct to interface dependecy loading filling in-memory repository from sql on creating new service new method to Add [s Sim]

--- internal/services/sim_service.go
+++ internal/services/sim_service.go
@@ -2,33 +2,55 @@ package services
 
 import (
 	"context"
+	"log"
 	"simactive/internal/core"
-	"simactive/internal/repository"
 )
 
-type SimService struct {
-	inMemoryRepo *repository.SimInMemoryRepo
-	sqlRepo      *repository.SimSqlRepository
+type SimRepository interface {
+	Save(ctx context.Context, sim core.Sim) (id int, err error)
+	GetSimList(ctx context.Context) (list *core.SimList, err error)
 }
 
-func NewSimService(inMemRepository *repository.SimInMemoryRepo, sqlRepository *repository.SimSqlRepository) *SimService {
-	return &SimService{
+type SimService struct {
+	inMemoryRepo SimRepository
+	sqlRepo      SimRepository
+}
+
+func NewSimService(inMemRepository SimRepository, sqlRepository SimRepository) *SimService {
+	ss := &SimService{
 		inMemoryRepo: inMemRepository,
 		sqlRepo:      sqlRepository,
 	}
-}
 
+	ctx := context.Background()
+
+	simList, err := ss.sqlRepo.GetSimList(ctx)
+	if err != nil {
+		log.Fatalf("Fatal error on getting sim list from sql: %v", err)
+	}
+
+	for _, sim := range *simList {
+		if _, err := ss.inMemoryRepo.Save(ctx, sim); err != nil {
+			log.Fatalf("Error on loading database into memory: %v", err)
+		}
+	}
+
+	log.Printf("Loaded %d simcards into memory", len(*simList))
+
+	return ss
+}
 func (ss *SimService) Add(ctx context.Context, s core.Sim) error {
-	id, err := ss.sqlRepo.Save(s)
+	id, err := ss.sqlRepo.Save(ctx, s)
 	if err != nil {
 		return err
 	}
 
 	s.SetID(id)
 
-	if err = ss.inMemoryRepo.Save(s); err != nil {
+	if _, err = ss.inMemoryRepo.Save(ctx, s); err != nil {
 		return err
 	}
+
 	return nil
 }
 func (ss *SimService) GetByID(ctx context.Context, id int) (sim core.Sim, err error) {
