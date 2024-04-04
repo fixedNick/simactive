04c0479024e6340b6f534f10764e04ac4612db5a
fixedNick <kkos44911@gmail.com>
2024-04-04 20:03:27 +0300

    added domain layer models `Service` & `ServiceList`

--- internal/core/service.go
+++ internal/core/service.go
@@ -5,10 +5,25 @@ type Service struct {
 	name string
 }
 
+func NewService(id int, name string) Service {
+	return Service{
+		id:   id,
+		name: name,
+	}
+}
+
 func (s *Service) Id() int {
 	return s.id
 }
 
 func (s *Service) Name() string {
 	return s.name
+}
+
+func (s *Service) SetID(id int) {
+	s.id = id
+}
+
+func (s *Service) SetName(name string) {
+	s.name = name
 }
