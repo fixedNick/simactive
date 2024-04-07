7653c089f77ef66be55e2982a54a2547461ae4f2
fixedNick <kkos44911@gmail.com>
2024-04-07 14:33:11 +0300

    Core: `Provider` and `Used` implementing DBModel interface

--- internal/core/provider.go
+++ internal/core/provider.go
@@ -1,9 +1,13 @@
 package core
 
+import "database/sql"
+
 type Provider struct {
 	id   int
 	name string
 }
+
+///  Getters
 
 func (p *Provider) Id() int {
 	return p.id
@@ -11,4 +15,34 @@ func (p *Provider) Id() int {
 
 func (p *Provider) Name() string {
 	return p.name
+}
+
+/// Setters
+
+func (p *Provider) SetId(id int) {
+	p.id = id
+}
+
+func (p *Provider) SetName(name string) {
+	p.name = name
+}
+
+// [Scan] return object of [Sim] whitch is [Scannable], and map index [int]
+// If any errors ocured while scanning it will be in [error]
+func (p *Provider) ScanRows(row *sql.Rows) (int, error) {
+	err := row.Scan(&p.id, &p.name)
+	return p.id, err
+}
+
+func (p *Provider) ScanRow(row *sql.Row) error {
+	err := row.Scan(&p.id, &p.name)
+	return err
+}
+
+func (p *Provider) GetKey() int {
+	return p.Id()
+}
+
+func (p *Provider) SetKey(id int) {
+	p.id = id
 }
--- internal/core/used.go
+++ internal/core/used.go
@@ -1,21 +1,67 @@
 package core
 
+import "database/sql"
+
 type Used struct {
-	id        int
-	simId     int
-	serviceId int
-	blocked   bool
+	id          int
+	simId       int
+	serviceId   int
+	blocked     bool
+	blockedInfo string
 }
 
-func (u Used) Id() int {
+/// Getters
+
+func (u *Used) Id() int {
 	return u.id
 }
-func (u Used) SimID() int {
+func (u *Used) SimID() int {
 	return u.simId
 }
-func (u Used) ServiceID() int {
+func (u *Used) ServiceID() int {
 	return u.serviceId
 }
-func (u Used) Blocked() bool {
+func (u *Used) Blocked() bool {
 	return u.blocked
+}
+
+func (u *Used) BlockedInfo() string {
+	return u.blockedInfo
+}
+
+/// Setters
+func (u *Used) SetId(id int) {
+	u.id = id
+}
+func (u *Used) SetSimID(simID int) {
+	u.simId = simID
+}
+func (u *Used) SetServiceID(sid int) {
+	u.serviceId = sid
+}
+func (u *Used) SetBlocked(status bool) {
+	u.blocked = status
+}
+func (u *Used) SetBlockedInfo(binfo string) {
+	u.blockedInfo = binfo
+}
+
+// [Scan] return object of [Sim] whitch is [Scannable], and map index [int]
+// If any errors ocured while scanning it will be in [error]
+func (u *Used) ScanRows(row *sql.Rows) (int, error) {
+	err := row.Scan(&u.id, &u.simId, &u.serviceId, &u.blocked, &u.blockedInfo)
+	return u.id, err
+}
+
+func (u *Used) ScanRow(row *sql.Row) error {
+	err := row.Scan(&u.id, &u.simId, &u.serviceId, &u.blocked, &u.blockedInfo)
+	return err
+}
+
+func (u *Used) GetKey() int {
+	return u.Id()
+}
+
+func (u *Used) SetKey(id int) {
+	u.id = id
 }
