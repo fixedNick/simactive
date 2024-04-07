f7ae62055d8510cd9b9f682aa7d669ca35bad3d1
fixedNick <kkos44911@gmail.com>
2024-04-07 23:59:19 +0300

    added godocs for `Sim` and `Service` domain models

--- internal/core/service.go
+++ internal/core/service.go
@@ -14,37 +14,66 @@ func NewService(id int, name string) Service {
 	}
 }
 
+// Id returns service ID.
 func (s *Service) Id() int {
 	return s.id
 }
 
+// Name returns service name.
 func (s *Service) Name() string {
 	return s.name
 }
 
+// SetID sets service ID.
 func (s *Service) SetID(id int) {
 	s.id = id
 }
 
+// SetName sets service name.
 func (s *Service) SetName(name string) {
 	s.name = name
 }
 
-// [ScanRow] return object of [Service] whitch is [Scannable], and map index [int]
-// If any errors ocured while scanning it will be in [error]
-func (s *Service) ScanRow(rows *sql.Row) error {
-	err := rows.Scan(&s.id, &s.name)
+// WithName sets the name of the service.
+//
+// name: the name to set for the service.
+// Service: the updated service with the new name set.
+func (s Service) WithName(name string) Service {
+	s.name = name
+	return s
+}
+
+// ScanRow scans the row into the service struct.
+// It returns an error if the scan fails.
+//
+// row: the sql rows to scan.
+// error: the error, if any, that occurred during the scan.
+func (s *Service) ScanRow(row *sql.Row) error {
+	err := row.Scan(&s.id, &s.name)
 	return err
 }
+
+// ScanRows scans the rows into the service struct.
+// It returns the id of the service and an error if the scan fails.
+//
+// rows: the sql rows to scan.
+// id: the id of the service.
+// error: the error, if any, that occurred during the scan.
 func (s *Service) ScanRows(rows *sql.Rows) (int, error) {
 	err := rows.Scan(&s.id, &s.name)
 	return s.id, err
 }
 
+// GetKey returns the map key of the Service that used in the List.
+//
+// id: the id of the service.
 func (s *Service) GetKey() int {
 	return s.Id()
 }
 
+// SetKey sets the id of the service.
+//
+// id: the id of the service.
 func (s *Service) SetKey(id int) {
 	s.id = id
 }
--- internal/core/sim.go
+++ internal/core/sim.go
@@ -11,70 +11,74 @@ type Sim struct {
 	activateUntil int64
 }
 
+// NewSim creates a new Sim object with the given parameters.
+// Parameters:
+//   - id: the id of the Sim.
+//   - number: the number of the Sim.
+//   - providerId: the provider id of the Sim.
+//   - isActivated: a boolean indicating if the Sim is activated.
+//   - activateUntil: the timestamp until when the Sim is activated.
+//   - isBlocked: a boolean indicating if the Sim is blocked.
+// Returns:
+//   - Sim: the newly created Sim object.
 func NewSim(id int, number string, providerId int, isActivated bool, activateUntil int64, isBlocked bool) Sim {
+	// Create and return a new Sim object with the given parameters.
 	return Sim{
-		id:            id,
-		number:        number,
-		providerId:    providerId,
-		isActivated:   isActivated,
-		activateUntil: activateUntil,
-		isBlocked:     isBlocked,
+		id:            id,            // Set the id of the Sim.
+		number:        number,        // Set the number of the Sim.
+		providerId:    providerId,    // Set the provider id of the Sim.
+		isActivated:   isActivated,   // Set if the Sim is activated.
+		activateUntil: activateUntil, // Set the timestamp until when the Sim is activated.
+		isBlocked:     isBlocked,     // Set if the Sim is blocked.
 	}
 }
-func (s Sim) Id() int {
-	return s.id
-}
-func (s Sim) Number() string {
-	return s.number
-}
-func (s Sim) ProviderID() int {
-	return s.providerId
-}
-func (s Sim) IsBlocked() bool {
-	return s.isBlocked
-}
-func (s Sim) IsActivated() bool {
-	return s.isActivated
-}
-func (s Sim) ActivateUntil() int64 {
-	return s.activateUntil
-}
 
-func (s *Sim) SetID(id int) {
-	s.id = id
-}
-func (s *Sim) SetActivated(status bool) {
-	s.isActivated = status
-}
-func (s *Sim) SetNumber(number string) {
-	s.number = number
-}
-func (s *Sim) SetBlocked(status bool) {
-	s.isBlocked = status
-}
-func (s *Sim) SetActivateUntil(aunt int64) {
-	s.activateUntil = aunt
-}
-func (s *Sim) SetProviderID(pid int) {
-	s.providerId = pid
-}
+// Getters
 
-// [Scan] return object of [Sim] whitch is [Scannable], and map index [int]
-// If any errors ocured while scanning it will be in [error]
+func (s Sim) Id() int              { return s.id }
+func (s Sim) Number() string       { return s.number }
+func (s Sim) ProviderID() int      { return s.providerId }
+func (s Sim) IsBlocked() bool      { return s.isBlocked }
+func (s Sim) IsActivated() bool    { return s.isActivated }
+func (s Sim) ActivateUntil() int64 { return s.activateUntil }
+
+// Setters
+
+func (s *Sim) SetID(id int)                { s.id = id }
+func (s *Sim) SetNumber(number string)     { s.number = number }
+func (s *Sim) SetProviderID(pid int)       { s.providerId = pid }
+func (s *Sim) SetBlocked(status bool)      { s.isBlocked = status }
+func (s *Sim) SetActivated(status bool)    { s.isActivated = status }
+func (s *Sim) SetActivateUntil(aunt int64) { s.activateUntil = aunt }
+
+// ScanRow scans the values from the given sql.Rows into the fields of the Sim struct.
+//
+// It takes a pointer to a sql.Row as parameter and returns an error.
+// Scanned values are stored in the fields of the pointer to the Sim struct.
 func (s *Sim) ScanRows(row *sql.Rows) (int, error) {
 	err := row.Scan(&s.id, &s.number, &s.providerId, &s.isActivated, &s.activateUntil, &s.isBlocked)
 	return s.id, err
 }
 
+// ScanRow scans the values from the given sql.Row into the fields of the Sim struct.
+//
+// It takes a pointer to a sql.Row as parameter and returns an error.
+// Scanned values are stored in the fields of the pointer to the Sim struct.
 func (s *Sim) ScanRow(row *sql.Row) error {
 	err := row.Scan(&s.id, &s.number, &s.providerId, &s.isActivated, &s.activateUntil, &s.isBlocked)
 	return err
 }
 
+// GetKey returns the map key of the Sim that used in the List.
+//
+// Returns an integer.
 func (s *Sim) GetKey() int {
 	return s.Id()
 }
 
+// SetKey sets the id for the Sim struct.
+//
+// Parameter: id int - the new id to set.
 func (s *Sim) SetKey(id int) {
 	s.id = id
 }
